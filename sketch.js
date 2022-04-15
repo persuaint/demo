10a7ab9b837c41f1a2ed68e38d227732155ed041
persuaint <103782213+persuaint@users.noreply.github.com>
2022-04-15 15:48:04 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,31 @@
+var box
+function setup() {
+  createCanvas(400,400);
+  box=createSprite(200,200,30,30)
+}
+
+function draw() 
+{
+  background("green");
+  if(keyDown("right")){
+    box.x=box.x+5
+  }
+
+  if(keyDown("left")){
+    box.x=box.x-5
+  }
+
+  if(keyDown("up")){
+    box.y=box.y-5
+  }
+
+  if(keyDown("down")){
+    box.y=box.y+5
+  }
+  drawSprites()
+
+}
+
+
+
+
